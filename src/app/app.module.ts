eb264c7059284a10244cdfcfdc8ce3ea1724d4eb
Alejandro González Reyes <alexglzrys@gmail.com>
2022-01-22 14:40:43 -0600

    instalar librería de sockets en el cliente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { environment } from 'src/environments/environment';
+
+// Socket IO
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+const config: SocketIoConfig = { url: environment.wsURL, options: {} };
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +14,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
